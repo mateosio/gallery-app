3d21260661115c61fd41cf32a017ea2852dc26df
Mateo Sio <siomateo8@gmail.com>
2023-08-22 22:50:52 -0300

    Agregue alert por foto agregada a favorito y su visualización

--- src/component/DisplayPhotos.jsx
+++ src/component/DisplayPhotos.jsx
@@ -1,14 +1,32 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
 import { selectPhotos } from "../features/photosSlice";
 import { useSelector } from "react-redux";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import '../component/DisplayPhotos.css';
+import { useDispatch } from "react-redux";
+import { addFavorite } from "../features/favoritePhotosSlice";
+
+import {Alert, Stack} from '@mui/material';
+
+
+
 
 export function DisplayPhotos (){
+
+    const [showAlert, setShowAlert] = useState(false);
     
     const photos = useSelector(selectPhotos);
-    console.log(photos);
+    const dispatch = useDispatch();
     
+    const handleFavorite = (photo) =>{
+        dispatch(addFavorite(photo))
+        setShowAlert(true)
+
+        setTimeout(()=>{
+            setShowAlert(false)
+        }, 2000)
+    };
+  
     
         return(
             <div className="photos_container">
@@ -16,12 +34,18 @@ export function DisplayPhotos (){
                     {photos.map(photo => (
                         <div className="image-container">
                             <img className="img" key={photo.id} src={photo.urls.regular} alt={photo.alt_description}></img>
-                            <div className="icon-container">
+                            <div className="icon-container" onClick={()=>{ handleFavorite(photo) }}>
                                 <FavoriteIcon />
                             </div>
                         </div>
 
                     ))}
+
+                    {showAlert && 
+                    <Stack className="alert_container">
+                        <Alert sx={{ maxWidth: '100%', minWidth: "100%" }} severity="success">Added to favorite</Alert>
+                    </Stack>
+                    }
                    
             </div>
         )       
--- src/component/MyPhotos.jsx
+++ src/component/MyPhotos.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { selectFavoritePhotos } from "../features/favoritePhotosSlice";
+import { useSelector } from "react-redux";
+import DeleteIcon from '@mui/icons-material/Delete';
+import "../component/MyPhotos.css"
 
 export function MyPhotos () {
 
+    const favoritePhotos = useSelector(selectFavoritePhotos);
+    console.log(favoritePhotos);
     return ( 
     <>
         <Navbar />
-        <h1>Favorite</h1>
+        <div className="favorite_container">
+            <div className="favoritePhotos_container">
+                
+                    {favoritePhotos.map(photo => (
+                        <div className="image-container">
+                            <img className="img" key={photo.id} src={photo.urls.regular} alt={photo.alt_description}></img>
+                            <div className="icon-container" >
+                                <DeleteIcon />
+                            </div>
+                        </div>
+
+                    ))}
+                   
+            </div>
+        
+        </div>
+        <Footer />        
         
     </>
 )
